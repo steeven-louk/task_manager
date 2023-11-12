8d8032b1c6290a52f9c17a018f54dbcf8f21ee4a
steeven-louk <steevenlouk.sl@gmail.com>
2023-11-12 15:31:02 +0100

    mise en place temporaire du dashboard

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
         <div className="head w-full ">
             <div className="date justify-between flex align-baseline">
-                <span>Today, 24 July</span>
+                <span className='font-extrabold'>Today, 24 July</span>
                 <div className="user inline-flex gap-3">
                     <span className='border p-3'>img</span>
                     <span>name</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,88 @@ export default function Home() {
   return (
     <main className="min-h-screen w-full stroke-lime-100">
       <Header/>
+      <div className="flex gap-4 mt-5">
+        <div className="recent-projet flex-1">
+          <div className="head flex w-full mb-5 justify-between">
+            <h2 className="font-bold">Recent projets</h2>
+            <span className=" text-gray-300">view all</span>
+          </div>
+
+          <div className="projet flex gap-3 ">
+            <div className="projet-1 bg-pink-400 p-4 rounded-md w-fit">
+                <h2 className="font-bold">Coinc calc</h2>
+                <span>category</span>
+            </div>
+
+            <div className="projet-1 bg-pink-400 p-4 rounded-md w-fit">
+                <h2 className="font-bold">Coinc calc</h2>
+                <span>category</span>
+            </div>
+
+            <div className="projet-1 bg-pink-400 p-4 rounded-md w-1/2">
+                <h2 className="font-bold">Coinc calc</h2>
+                <span>category</span>
+            </div>
+
+            <div className="projet-1 bg-pink-400 p-4 rounded-md w-1/2">
+                <h2 className="font-bold">Coinc calc</h2>
+                <span>category</span>
+            </div>
+
+            <button className="projet-1 bg-gray-400 hover:bg-gray-300 transition-all p-4 rounded-md w-1/2">
+                add project
+            </button>
+          </div>
+        </div>
+        <div className="recent-task flex-1">
+        <div className="head flex w-full mb-5 justify-between">
+            <h2 className="font-bold">Recent tasks</h2>
+            <span className="text-gray-300">view all</span>
+          </div>
+
+          <div className="task_container flex flex-col gap-4">
+              <div className="task shadow p-2">
+                <div className="head flex justify-between">
+                  <h2 className="font-bold">titlle</h2>
+                  <span>view</span>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet consectetur.
+                </div>
+              </div>
+
+              <div className="task shadow p-2">
+                <div className="head flex justify-between">
+                  <h2 className="font-bold">titlle</h2>
+                  <span>view</span>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet consectetur.
+                </div>
+              </div>
+
+              <div className="task shadow p-2">
+                <div className="head flex justify-between">
+                  <h2 className="font-bold">titlle</h2>
+                  <span>view</span>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet consectetur.
+                </div>
+              </div>
+
+              <div className="task shadow p-2">
+                <div className="head flex justify-between">
+                  <h2 className="font-bold">titlle</h2>
+                  <span>view</span>
+                </div>
+                <div className="desc">
+                  Lorem ipsum dolor sit amet consectetur.
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
