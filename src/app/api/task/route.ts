10d9435fa104dd54695bfcfae66f310d12ab1166
steeven-louk <steevenlouk.sl@gmail.com>
2023-11-23 18:08:23 +0100

    mise en place de l'api de modification et suppression d'une task

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,4 +1,5 @@
 // import {  } from 'next/server.js';
+import Task from "@/app/components/ShowTask";
 import connectMongoDb from "@/app/libs/db";
 import Tasks from "@/app/libs/models/Tasks";
 import { NextResponse , NextRequest} from "next/server";
@@ -11,7 +12,7 @@ export async function GET(){
         const tasks = await Tasks.find({});
         return NextResponse.json({tasks}, {status:200});
     } catch (error) {
-        return NextResponse.json({error: error},{status:500});
+        return NextResponse.json({error},{status:500});
     }
 }
 
@@ -26,6 +27,46 @@ export async function POST(NextRequest:any){
 
     } catch (error) {
         console.log(error)
-        return NextResponse.json({error:error},{status:500});
+        return NextResponse.json({error},{status:500});
+    }
+}
+
+
+export async function PUT(NextRequest:any){
+    await connectMongoDb().catch((error)=> console.log(error));
+
+    try {
+        const body = await NextRequest.json();
+        const existingTask = await Tasks.findById(body._id);
+        if(!existingTask){
+            return NextResponse.json({"error":"la task n'existe pas"}, {status:404})
+        }
+        const updateTask = await Tasks.findByIdAndUpdate(body._id, {$set:body}, {new: true})
+        return NextResponse.json({updateTask},{status: 200});
+    } catch (error) {
+        console.log("update error", error);
+        console.log("update error", "errur lors de l'ajout de la task");
+        return NextResponse.json({error}, {status:500})
+    }
+}
+
+export async function DELETE(NextRequest:any){
+
+    await connectMongoDb().catch((error)=> console.log(error));
+
+    try {
+        const body = await NextRequest.json();
+        const existingTask = await Tasks.findById(body._id);
+        if(!existingTask){
+            console.log("task non trouver");
+            return NextResponse.json({"error":"task not found"}, {status:404});
+        }
+
+        const task = await Tasks.findByIdAndDelete(existingTask._id);
+        return NextResponse.json({task}, {status:200});
+
+    } catch (error) {
+        console.log("error", error);
+        return NextResponse.json({error}, {status:500})
     }
 }
